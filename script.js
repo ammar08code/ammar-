32d823e323ff039e3389f7700adeececba4f8440
AMMAR <ammartharith@gmail.com>
2026-08-22 00:08:13 +0800

    reeveal

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function tekanButton() {
-    alert("Welcome to AMMAR's Website!");
-}
+const sections = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+sections.forEach((section) => {
+    observer.observe(section);
+});
